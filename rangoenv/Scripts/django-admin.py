9b71770e22c01caa39d3846e749c013df0c5ae52
pierre outters <2854390o@student.gla.ac.uk>
2025-01-29 18:04:18 +0000

    Chapter 3.5

---
+++ rangoenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\pierr\OneDrive\Desktop\Pierre-Alexis\Uni_2nd_year\WAD_2\Workspace\tango_with_django_project\rangoenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
