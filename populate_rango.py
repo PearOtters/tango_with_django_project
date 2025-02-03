8a9d085c7ae7e878e5defbd6ba722b67f121a28d
pierre outters <2854390o@student.gla.ac.uk>
2025-02-03 12:17:57 +0000

    Tests pass Chapter 6.3

--- populate_rango.py
+++ populate_rango.py
@@ -21,7 +21,7 @@ def populate():
     
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01',
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
          'views':18},
         {'title':'Django Rocks',
          'url':'http://www.djangorocks.com/',
